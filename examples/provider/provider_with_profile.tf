ef4c9e216a45253dbb992de9fbf1b86cff53a4a9
enis <enis@cloudera.com>
2023-06-02 06:42:31 -0700

    CDPCP-9564 [Terraform provider] Add copyright headers (#60)
    Added Copyright headers to all .go .tf .yaml and Makefiles. Added a unit test for checking
    whether these files have the copyright blurp or not. We skip generated files.

--- examples/provider/provider_with_profile.tf
+++ examples/provider/provider_with_profile.tf
@@ -1,3 +1,13 @@
+## Copyright 2023 Cloudera. All Rights Reserved.
+#
+# This file is licensed under the Apache License Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. Refer to the License for the specific
+# permissions and limitations governing your use of the file.
+
 # This example shows how to use cdp with a custom profile (other than 'default').
 # The profile must be defined in the CDP configuration file (default: ~/.cdp/config) and credentials should be available
 # under ~/.cdp/credentials.
