a9772a93cec9cffbaa6f2a5f04a6b2066130356d
David Szabo <dszabo@cloudera.com>
2023-06-05 16:26:34 +0200

    CDPCP-9595 Azure Environment resource complete with CRUD operations

---
+++ examples/resources/cdp_environments_azure_environment/resource.tf
@@ -0,0 +1,57 @@
+## Copyright 2023 Cloudera. All Rights Reserved.
+#
+# This file is licensed under the Apache License Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. Refer to the License for the specific
+# permissions and limitations governing your use of the file.
+
+terraform {
+  required_providers {
+    cdp = {
+      source = "terraform.cloudera.com/cloudera/cdp"
+    }
+  }
+}
+
+resource "cdp_environments_azure_credential" "example-cred" {
+  credential_name = "example-cdp-azure-credential"
+  subscription_id = "<value>"
+  tenant_id       = "<value>"
+  app_based = {
+    application_id  = "<value>"
+    secret_key      = "<value>"
+  }
+  description     = "Example Azure Credential"
+}
+
+resource "cdp_environments_azure_environment" "example-env" {
+  environment_name = "example-cdp-azure-environment"
+  credential_name  = cdp_environments_azure_credential.example-cred.credential_name
+  region           = "us-west"
+  security_access = {
+    cidr = "0.0.0.0/0"
+  }
+  existing_network_params = {
+    network_id = "network-name"
+    resource_group_name = "rg-name"
+    subnet_ids = ["subnet.id"]
+  }
+  public_key = "my-key"
+  log_storage = {
+    storage_location_base = "abfs://rgname-fs@rgname-storage.dfs.core.windows.net"
+    managed_identity      = "/subscriptions/123e4567-e89b-12d3-a456-426614174000/resourcegroups/my-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/logger"
+  }
+  resource_group_name = "rg-name"
+  use_public_ip = true
+}
+
+output "environment_name" {
+  value = cdp_environments_azure_environment.example-env.environment_name
+}
+
+output "crn" {
+  value = cdp_environments_azure_environment.example-env.crn
+}
---
+++ resources/environments/model_azure_environment.go
@@ -0,0 +1,103 @@
+// Copyright 2023 Cloudera. All Rights Reserved.
+//
+// This file is licensed under the Apache License Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, either express or implied. Refer to the License for the specific
+// permissions and limitations governing your use of the file.
+
+package environments
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type azureEnvironmentResourceModel struct {
+	ID types.String `tfsdk:"id"`
+
+	Crn types.String `tfsdk:"crn"`
+
+	CreatePrivateEndpoints types.Bool `tfsdk:"create_private_endpoints"`
+
+	CredentialName types.String `tfsdk:"credential_name"`
+
+	Description types.String `tfsdk:"description"`
+
+	EnableOutboundLoadBalancer types.Bool `tfsdk:"enable_outbound_load_balancer"`
+
+	EnableTunnel types.Bool `tfsdk:"enable_tunnel"`
+
+	EncryptionKeyResourceGroupName types.String `tfsdk:"encryption_key_resource_group_name"`
+
+	EncryptionKeyURL types.String `tfsdk:"encryption_key_url"`
+
+	EnvironmentName types.String `tfsdk:"environment_name"`
+
+	ExistingNetworkParams types.Object `tfsdk:"existing_network_params"`
+
+	FreeIpa types.Object `tfsdk:"freeipa"`
+
+	LogStorage *azureLogStorage `tfsdk:"log_storage"`
+
+	NewNetworkParams types.Object `tfsdk:"new_network_params"`
+
+	ProxyConfigName types.String `tfsdk:"proxy_config_name"`
+
+	PublicKey types.String `tfsdk:"public_key"`
+
+	Region types.String `tfsdk:"region"`
+
+	ReportDeploymentLogs types.Bool `tfsdk:"report_deployment_logs"`
+
+	ResourceGroupName types.String `tfsdk:"resource_group_name"`
+
+	SecurityAccess *SecurityAccess `tfsdk:"security_access"`
+
+	Status types.String `tfsdk:"status"`
+
+	StatusReason types.String `tfsdk:"status_reason"`
+
+	Tags types.Map `tfsdk:"tags"`
+
+	UsePublicIP types.Bool `tfsdk:"use_public_ip"`
+
+	WorkloadAnalytics types.Bool `tfsdk:"workload_analytics"`
+}
+
+type existingAzureNetwork struct {
+	AksPrivateDNSZoneID types.String `tfsdk:"aks_private_dns_zone_id"`
+
+	DatabasePrivateDNSZoneID types.String `tfsdk:"database_private_dns_zone_id"`
+
+	NetworkID types.String `tfsdk:"network_id"`
+
+	ResourceGroupName types.String `tfsdk:"resource_group_name"`
+
+	SubnetIds types.Set `tfsdk:"subnet_ids"`
+}
+
+type azureFreeIpaDetails struct {
+	Catalog types.String `tfsdk:"catalog"`
+
+	ImageID types.String `tfsdk:"image_id"`
+
+	InstanceCountByGroup types.Int64 `tfsdk:"instance_count_by_group"`
+
+	InstanceType types.String `tfsdk:"instance_type"`
+
+	Recipes types.Set `tfsdk:"recipes"`
+}
+
+type azureLogStorage struct {
+	ManagedIdentity types.String `tfsdk:"managed_identity"`
+
+	StorageLocationBase types.String `tfsdk:"storage_location_base"`
+
+	BackupStorageLocationBase types.String `tfsdk:"backup_storage_location_base"`
+}
+
+type newNetworkParams struct {
+	NetworkCidr types.String `tfsdk:"network_cidr"`
+}
