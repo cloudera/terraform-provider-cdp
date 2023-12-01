dbdf29013b4aef7deac6a430718570f24645877f
Mészáros Gergely <grigorijat9026@gmail.com>
2023-12-01 16:56:01 +0100

    CDPCP-10967 - GCP environment creation Terraform Resource fails due to incorrect value returned for storage location base

--- resources/environments/converter_gcp_test.go
+++ resources/environments/converter_gcp_test.go
@@ -12,13 +12,20 @@ package environments
 
 import (
 	"context"
+	"github.com/go-openapi/strfmt"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"testing"
 
+	environmentsmodels "github.com/cloudera/terraform-provider-cdp/cdp-sdk-go/gen/environments/models"
 	"github.com/cloudera/terraform-provider-cdp/utils"
 )
+
+type GcpEnvironmentResourceModelMock struct {
+	mock.Mock
+}
 
 func TestToGcpEnvironmentRequestRootFields(t *testing.T) {
 	testObject := createFilledGcpEnvironmentResourceModel()
@@ -79,6 +86,27 @@ func TestToGcpEnvironmentRequestFreeIpa(t *testing.T) {
 	assert.Equal(t, testObject.FreeIpa.InstanceType.ValueString(), result.FreeIpa.InstanceType)
 }
 
+func TestToGcpEnvironmentResourceRootFields(t *testing.T) {
+	testModel := &gcpEnvironmentResourceModel{}
+	testEnv := createFilledEnvironment()
+
+	toGcpEnvironmentResource(context.TODO(), testEnv, testModel, getTestPollingOption(), nil)
+
+	stringExists(t, *testEnv.Crn, testModel.Crn)
+	stringExists(t, *testEnv.Region, testModel.Region)
+	stringExists(t, *testEnv.Status, testModel.Status)
+	stringExists(t, testEnv.Description, testModel.Description)
+	stringExists(t, testEnv.StatusReason, testModel.StatusReason)
+	stringExists(t, *testEnv.CredentialName, testModel.CredentialName)
+	stringExists(t, *testEnv.EnvironmentName, testModel.EnvironmentName)
+}
+
+func stringExists(t *testing.T, expected string, underTest types.String) {
+	assert.True(t, !underTest.IsNull())
+	assert.True(t, !underTest.IsUnknown())
+	assert.Equal(t, expected, underTest.ValueString())
+}
+
 func createFilledGcpEnvironmentResourceModel() *gcpEnvironmentResourceModel {
 	return &gcpEnvironmentResourceModel{
 		EnvironmentName: types.StringValue("someEnvironmentName"),
@@ -120,6 +148,148 @@ func createFilledGcpEnvironmentResourceModel() *gcpEnvironmentResourceModel {
 		Status:                      types.StringValue("someStatus"),
 		StatusReason:                types.StringValue("someStatusReason"),
 	}
+}
+
+func createFilledEnvironment() *environmentsmodels.Environment {
+	return &environmentsmodels.Environment{
+		Authentication: &environmentsmodels.Authentication{
+			LoginUserName: "someLoginUserName",
+			PublicKey:     "somePublicKey",
+			PublicKeyID:   "somePublicKeyID",
+		},
+		AwsDetails: &environmentsmodels.EnvironmentAwsDetails{},
+		BackupStorage: &environmentsmodels.BackupStorage{
+			AwsDetails: &environmentsmodels.BackupStorageAwsDetails{
+				InstanceProfile:     "someInstanceProfile",
+				StorageLocationBase: "someStorageLocationBase",
+			},
+			AzureDetails: &environmentsmodels.BackupStorageAzureDetails{
+				ManagedIdentity:     "someManagedIdentity",
+				StorageLocationBase: "someStorageLocationBase",
+			},
+			Enabled: true,
+			GcpDetails: &environmentsmodels.BackupStorageGcpDetails{
+				ServiceAccountEmail: "someServiceAccountEmail",
+				StorageLocationBase: "someStorageLocationBase",
+			},
+		},
+		CloudPlatform:   func(s string) *string { return &s }("someCloudPlatform"),
+		Created:         strfmt.NewDateTime(),
+		Creator:         "someone",
+		CredentialName:  func(s string) *string { return &s }("someCredential"),
+		Crn:             func(s string) *string { return &s }("someCrn"),
+		Description:     "someDescription",
+		EnvironmentName: func(s string) *string { return &s }("someEnvironmentName"),
+		Freeipa: &environmentsmodels.FreeipaDetails{
+			Crn:      "someFreeIpaCrn",
+			Domain:   "someDomain",
+			Hostname: "someHostname",
+			Instances: []*environmentsmodels.FreeIpaInstance{{
+				AttachedVolumes: []*environmentsmodels.AttachedVolumeDetail{
+					{
+						Count:      1,
+						Size:       1,
+						VolumeType: "someVolumeType",
+					},
+				},
+				AvailabilityZone:     "someAvailabilityZone",
+				DiscoveryFQDN:        "someDiscoveryFQDN",
+				InstanceGroup:        "someInstanceGroup",
+				InstanceID:           "someInstanceID",
+				InstanceStatus:       "someInstanceStatus",
+				InstanceStatusReason: "someInstanceStatusReason",
+				InstanceType:         "someInstanceType",
+				InstanceVMType:       "someInstanceVMType",
+				LifeCycle:            "someLifeCycle",
+				PrivateIP:            "somePrivateIP",
+				PublicIP:             "somePublicIP",
+				SSHPort:              1,
+				SubnetID:             "someSubnetID",
+			}},
+			Recipes:  []string{"someFreeIpaRecipe"},
+			ServerIP: []string{"someServerIp"},
+		},
+		GcpDetails: &environmentsmodels.EnvironmentGcpDetails{},
+		LogStorage: &environmentsmodels.LogStorage{
+			AwsDetails: &environmentsmodels.LogStorageAwsDetails{
+				InstanceProfile:     "someInstanceProfile",
+				StorageLocationBase: "someStorageLocationBase",
+			},
+			AzureDetails: &environmentsmodels.LogStorageAzureDetails{
+				ManagedIdentity:     "someManagedIdentity",
+				StorageLocationBase: "someStorageLocationBase",
+			},
+			Enabled: func(b bool) *bool { return &b }(true),
+			GcpDetails: &environmentsmodels.LogStorageGcpDetails{
+				ServiceAccountEmail: "someServiceAccountEmail",
+				StorageLocationBase: "someStorageLocationBase",
+			},
+		},
+		Network: &environmentsmodels.Network{
+			Aws: &environmentsmodels.NetworkAwsParams{},
+			Azure: &environmentsmodels.NetworkAzureParams{
+				AksPrivateDNSZoneID:        "someAksPrivateDNSZoneID",
+				DatabasePrivateDNSZoneID:   "someDatabasePrivateDNSZoneID",
+				EnableOutboundLoadBalancer: true,
+				NetworkID:                  func(s string) *string { return &s }("someNetworkID"),
+				ResourceGroupName:          func(s string) *string { return &s }("someResourceGroupName"),
+				UsePublicIP:                func(b bool) *bool { return &b }(true),
+			},
+			EndpointAccessGatewayScheme:    "someEndpointAccessGatewayScheme",
+			EndpointAccessGatewaySubnetIds: []string{"someEndpointAccessGatewaySubnetId"},
+			Gcp: &environmentsmodels.NetworkGcpParams{
+				NetworkName:     func(s string) *string { return &s }("someNetworkName"),
+				SharedProjectID: "someSharedProjectID",
+				UsePublicIP:     func(b bool) *bool { return &b }(true),
+			},
+			NetworkCidr: "someNetworkCidr",
+			NetworkName: func(s string) *string { return &s }("someNetworkName"),
+			SubnetIds:   []string{"someSubnetId"},
+			SubnetMetadata: map[string]environmentsmodels.CloudSubnet{
+				"someSubnetMetadata": {
+					AvailabilityZone: "someAvailabilityZone",
+					Cidr:             "someCidr",
+					SubnetID:         "someSubnetID",
+					SubnetName:       "someSubnetName",
+				},
+			},
+		},
+		ProxyConfig: &environmentsmodels.ProxyConfig{
+			Crn:             func(s string) *string { return &s }("someProxyCrn"),
+			Description:     "someDescription",
+			Host:            func(s string) *string { return &s }("someHost"),
+			NoProxyHosts:    "someNoProxyHosts",
+			Password:        "somePassword",
+			Port:            func(i int32) *int32 { return &i }(int32(1)),
+			Protocol:        func(s string) *string { return &s }("someProtocol"),
+			ProxyConfigName: func(s string) *string { return &s }("someProxyConfigName"),
+			User:            "someUser",
+		},
+		Region:               func(s string) *string { return &s }("someRegion"),
+		ReportDeploymentLogs: true,
+		SecurityAccess: &environmentsmodels.SecurityAccess{
+			Cidr:                   "someCidr",
+			DefaultSecurityGroupID: "someDefaultSecurityGroupID",
+			SecurityGroupIDForKnox: "someSecurityGroupIDForKnox",
+		},
+		Status:       func(s string) *string { return &s }("someStatus"),
+		StatusReason: "someStatusReason",
+		Tags: &environmentsmodels.EnvironmentTags{
+			Defaults: map[string]string{
+				"someDefaultKey": "someDefaultValue",
+			},
+			UserDefined: map[string]string{
+				"someUserDefinedKey": "someUserDefinedValue",
+			},
+		},
+		TunnelEnabled:     true,
+		TunnelType:        environmentsmodels.TunnelTypeCCMV2,
+		WorkloadAnalytics: true,
+	}
+}
+
+func getTestPollingOption() *utils.PollingOptions {
+	return &utils.PollingOptions{PollingTimeout: types.Int64Value(1234)}
 }
 
 func createListOfStrings() types.List {
