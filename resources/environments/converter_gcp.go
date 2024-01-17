2497fcd7ebbc37f2b795794ade99a37d17225178
Mészáros Gergely <grigorijat9026@gmail.com>
2024-01-17 12:58:43 +0100

    CDPCP-10617 - Errors when specifying tags in CDP provider resources

--- resources/environments/converter_gcp.go
+++ resources/environments/converter_gcp.go
@@ -141,12 +141,8 @@ func toGcpEnvironmentResource(ctx context.Context, env *environmentsmodels.Envir
 	model.StatusReason = types.StringValue(env.StatusReason)
 	tflog.Info(ctx, "about to convert tags.")
 	if env.Tags != nil {
-		merged := env.Tags.Defaults
-		for k, v := range env.Tags.UserDefined {
-			merged[k] = v
-		}
 		var tagDiags diag.Diagnostics
-		tagMap, tagDiags := types.MapValueFrom(ctx, types.StringType, merged)
+		tagMap, tagDiags := types.MapValueFrom(ctx, types.StringType, env.Tags.UserDefined)
 		diags.Append(tagDiags...)
 		model.Tags = tagMap
 	}
