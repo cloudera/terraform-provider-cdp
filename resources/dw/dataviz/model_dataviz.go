61e7e473eb3820a3df2a66d7f1dc9211f63b57d1
Viktor Csomor <viq@cloudera.com>
2024-12-17 14:16:03 +0100

    CDPCP-13591 Cloudera Data Warehouse - Data Visualisation Support
    Add Data Visualization model

---
+++ resources/dw/dataviz/model_dataviz.go
@@ -0,0 +1,37 @@
+// Copyright 2024 Cloudera. All Rights Reserved.
+//
+// This file is licensed under the Apache License Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, either express or implied. Refer to the License for the specific
+// permissions and limitations governing your use of the file.
+
+package hive
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/cloudera/terraform-provider-cdp/utils"
+)
+
+type resourceModel struct {
+	ID        types.String `tfsdk:"id"`
+	ClusterID types.String `tfsdk:"cluster_id"`
+	Name      types.String `tfsdk:"name"`
+
+	ImageVersion     types.String `tfsdk:"image_version"`
+	ResourceTemplate types.String `tfsdk:"resource_template"`
+
+	UserGroups  types.List `tfsdk:"user_groups"`
+	AdminGroups types.List `tfsdk:"admin_groups"`
+
+	LastUpdated    types.String          `tfsdk:"last_updated"`
+	Status         types.String          `tfsdk:"status"`
+	PollingOptions *utils.PollingOptions `tfsdk:"polling_options"`
+}
+
+func (p *resourceModel) GetPollingOptions() *utils.PollingOptions {
+	return p.PollingOptions
+}
